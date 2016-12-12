1b2aef4b4a5ebdbe0f04055e53c69921ad5fb631
Dhilip <dhilip.kumar.s@huawei.com>
2016-12-12 12:19:23 +0530

    Implement data collection and report generation

--- redis_sentinel_micro.go
+++ redis_sentinel_micro.go
@@ -5,10 +5,11 @@ import (
 	"os"
 	"strings"
 	"sync"
-	"time"
+	"sort"
 
 	log "github.com/golang/glog"
 	"github.com/mediocregopher/radix.v2/redis"
+	"strconv"
 )
 
 const (
@@ -17,12 +18,120 @@ const (
 )
 
 type Redis struct {
-	EndPoint    string        //End point of the redis-server
-	Role        string        //Role of this redis-sever Master or Slave
-	LastUpdated time.Duration // When did last sync happened
-	SyncBytes   int64         //How much of data did this sync
-	SlaveOf     string        //Masters endpoint
-	Client      *redis.Client //Redis client
+	EndPoint         string        //End point of the redis-server
+	Role             string        //Role of this redis-sever Master or Slave
+	LastUpdated      int           // When did last sync happened seconds
+	MasterDownSince  int		//Since how long the master is not reachable?
+	SyncBytes        int64         //How much of data did this sync
+	MasterHost          string        //Masters ip addres
+	MasterPort	  string	 //Master port
+	Priority         int           //Slave priority
+	MasterLinkStatus bool          //true means up false mean down
+	Client           *redis.Client //Redis client
+}
+
+type RedisSlaves []*Redis
+
+func (rs RedisSlaves) Len() int {return len(rs)}
+func (rs RedisSlaves) Swap (i int, j int) {
+	var tmp *Redis
+	tmp = rs[i]
+	rs[i] = rs[j]
+	rs[j] = tmp
+}
+
+func (rs RedisSlaves) Less (i int, j int) bool {
+
+	if rs[i].Priority < rs[j].Priority {
+		return true
+	}
+
+	if rs[i].SyncBytes < rs[j].SyncBytes {
+		return true
+	}
+
+	if rs[i].LastUpdated > rs[j].LastUpdated {
+		return true
+	}
+
+	return false
+}
+
+func (R *Redis) ParseResponse(Res string) bool {
+
+	res := strings.Split(Res, "\\r\\n")
+	if len(res) == 1 {
+		log.Info("ParseResponse(): Invalid Redis-server response. Nothing to parse")
+		return false
+	}
+
+	for _, field := range res {
+
+		kv := strings.Split(field, ":")
+		if len(kv) == 2 {
+			switch kv[0] {
+			case "role":
+				R.Role = kv[1]
+			case "master_host":
+				R.MasterHost = kv[1]
+			case "master_port":
+				R.MasterPort = kv[1]
+			case "slave_repl_offset":
+				i, err := strconv.Atoi(kv[1])
+				if err == nil {
+					R.SyncBytes = int64(i)
+				}
+			case "master_link_down_since_seconds":
+				i, err := strconv.Atoi(kv[1])
+				if err == nil {
+					R.MasterDownSince = i
+				}
+			case "master_link_status":
+				if kv[1] == "on" {
+					R.MasterLinkStatus = true
+				} else {
+					R.MasterLinkStatus = false
+				}
+			case "master_last_io_seconds_ago":
+				i, err := strconv.Atoi(kv[1])
+				if err == nil {
+					R.LastUpdated = i
+				}
+			case "slave_priority":
+				i, err := strconv.Atoi(kv[1])
+				if err == nil {
+					R.Priority = i
+				}
+			}
+		}
+	}
+	fmt.Printf("R=%v\n", R)
+	return true
+}
+
+//CollectStats This function will take the endpoint
+func CollectStats(EndPoint string) (*Redis, error) {
+
+	var R Redis
+
+	IpPort := strings.Split(EndPoint, ":")
+	if len(IpPort) != 2 {
+		return nil, fmt.Errorf(COLLECTSTATS_INVALID_INPUT)
+	}
+
+	//Try to connect to the redis-servers
+	C, err := redis.Dial("tcp", EndPoint)
+	if err != nil {
+		log.Infof("CollectStats() %s Error:%v", COLLECTSTATS_SERVER_NOT_REACHABLE, err)
+		return nil, fmt.Errorf(COLLECTSTATS_SERVER_NOT_REACHABLE)
+	}
+	Res := C.Cmd("INFO", "REPLICATION")
+
+	//log.Infof("CollectStats(%s)=%v", EndPoint, Res.String())
+	R.ParseResponse(Res.String())
+
+	R.Client = C
+	return &R, nil
 }
 
 //
@@ -47,34 +156,15 @@ func CollectStatsAll(EndPoints []string) []*Redis {
 				log.Warningf("Error collecting stats for %v Error=%v", S, err)
 			}
 		}(S)
-
 	}
 	wg.Wait()
 	return Servers
 }
 
-//CollectStats This function will take the endpoint
-func CollectStats(EndPoint string) (*Redis, error) {
+func GuessMaster(Servers []*Redis) {
 
-	var R Redis
+	sort.Sort(RedisSlaves(Servers))
 
-	IpPort := strings.Split(EndPoint, ":")
-	if len(IpPort) != 2 {
-		return nil, fmt.Errorf(COLLECTSTATS_INVALID_INPUT)
-	}
-
-	//Try to connect to the redis-servers
-	C, err := redis.Dial("tcp", EndPoint)
-	if err != nil {
-		log.Infof("CollectStats() %s Error:%v", COLLECTSTATS_SERVER_NOT_REACHABLE, err)
-		return nil, fmt.Errorf(COLLECTSTATS_SERVER_NOT_REACHABLE)
-	}
-	Res := C.Cmd("INFO", "REPLICATION")
-
-	log.Infof("CollectStats(%s)=%v", EndPoint, Res.String())
-
-	R.Client = C
-	return &R, nil
 }
 
 func main() {
@@ -90,6 +180,9 @@ func main() {
 	log.Infof("Available Servers are %v", Servers)
 
 	//Does it really need a master
+	GuessMaster(Servers)
+
+	log.Infof("Sorted Servers are %v", Servers)
 
 	log.Infof("Redis-Sentinal-micro Finished")
 
--- redis_sentinel_micro_test.go
+++ redis_sentinel_micro_test.go
@@ -52,3 +52,44 @@ func TestCollectStatsAll_NoReachable(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestParseResponseNullInput(t *testing.T) {
+
+	var R Redis
+
+	resultBool := R.ParseResponse("")
+
+	t.Logf("ParseResponse =%v", resultBool)
+
+	if resultBool {
+		t.Fail()
+	}
+}
+
+func TestParseResponseValid(t *testing.T) {
+
+	var R Redis
+
+	sample_input := `# Replication\r\nrole:slave\r\nmaster_host:172.31.10.90\r\nmaster_port:6381\r\nmaster_link_status:up\r\nmaster_last_io_seconds_ago:9\r\nmaster_sync_in_progress:0\r\nslave_repl_offset:44983\r\nslave_priority:100\r\nslave_read_only:1\r\nconnected_slaves:0\r\nmaster_repl_offset:0\r\nrepl_backlog_active:0\r\nrepl_backlog_size:1048576\r\nrepl_backlog_first_byte_offset:0\r\nrepl_backlog_histlen:0\r\n`
+	if !R.ParseResponse(sample_input) {
+		t.Fail()
+	}
+
+	t.Logf("Valid ParseResult = %v", R)
+
+	if R.Role != "slave" {
+		t.Fail()
+	}
+	if R.Priority != 100 {
+		t.Fail()
+	}
+	if R.LastUpdated != 9 {
+		t.Fail()
+	}
+	if R.SyncBytes != 44983 {
+		t.Fail()
+	}
+
+	t.Logf("R=%v", R)
+
+}
